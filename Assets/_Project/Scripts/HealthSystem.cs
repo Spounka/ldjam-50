56baf13d493d009200601649c296734a2edb08eb
Nazih Boudaakkar <boudaakkranazih@gmail.com>
2022-04-02 15:17:06 +0100

    Added health system

--- Assets/_Project/Scripts/HealthSystem.cs
+++ Assets/_Project/Scripts/HealthSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,25 @@ namespace Spounka
     {
         #region Variables
 
-        [SerializeField] private int _maxHealth;
+        [SerializeField] private int _maxHealth = 100;
         private int _currentHealth;
 
+        public int MaxHealth => _maxHealth;
+
+        public int CurrentHealth => _currentHealth;
+
         #endregion
+
+        private void Awake()
+        {
+            _currentHealth = _maxHealth;
+        }
 
         public void TakeDamage(int damageAmount)
         {
             _currentHealth -= damageAmount;
+            if (_currentHealth <= 0)
+                _currentHealth = 0;
         }
     }
 }
