d71894027bce7d26c6e7119d50edbad9f7500df4
Nazih Boudaakkar <boudaakkranazih@gmail.com>
2022-04-02 15:17:34 +0100

    Added health ui system

--- Assets/_Project/Scripts/UI/HealthUI.cs
+++ Assets/_Project/Scripts/UI/HealthUI.cs
@@ -1,27 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Spounka
 {
     public class HealthUI : MonoBehaviour
     {
-    	#region Variables
-	
-	
-	
-    	#endregion
+        #region Variables
+
+        [SerializeField] private Slider _healthSlider;
+        [SerializeField] private HealthSystem _healthSystem;
+
+        #endregion
 
 
         private void Start()
         {
-        
+            _healthSlider.value = _healthSlider.maxValue = _healthSystem.MaxHealth;
         }
 
 
         private void Update()
         {
-        
+            _healthSlider.value = _healthSystem.CurrentHealth;
+        }
+
+        private void OnGUI()
+        {
+            if (GUI.Button(new Rect(100, 100, 200, 50), "Take Damage"))
+            {
+                _healthSystem.TakeDamage(10);
+            }
         }
     }
 }
