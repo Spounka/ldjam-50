c8b2761f8f82243654ee856ce40e1b4ac65d6480
Nazih Boudaakkar <boudaakkranazih@gmail.com>
2022-04-02 14:56:08 +0100

    Added basic shooting logic

--- Assets/_Project/Scripts/Shooting.cs
+++ Assets/_Project/Scripts/Shooting.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,40 @@ namespace Spounka
 {
     public class Shooting : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private float _shootingRate;
+        [SerializeField] private float _bulletSpeed;
+        [SerializeField] private Rigidbody2D _bulletPrefab;
+        private Transform _transform;
+        private float timer = 0;
+        private bool _mousePressed = false;
+
+        private bool CanShoot => _mousePressed && timer <= 0;
+
+
+        private void Start()
         {
-        
+            _transform = transform;
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-        
+            _mousePressed = Input.GetMouseButton(0);
+            if (timer > 0)
+                timer -= Time.deltaTime;
+        }
+
+        private void FixedUpdate()
+        {
+            if (CanShoot)
+                Shoot();
+        }
+
+        private void Shoot()
+        {
+            var @obj = Instantiate(_bulletPrefab, _transform.position, _transform.rotation);
+            @obj.AddForce(transform.up * _bulletSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            Destroy(@obj, 5.0f);
+            timer = _shootingRate;
         }
     }
 }
