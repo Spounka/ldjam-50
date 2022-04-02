7fb3b2bf1f8f5ca9bdd6515c9dfeb96351a29550
Nazih Boudaakkar <boudaakkranazih@gmail.com>
2022-04-02 16:30:01 +0100

    added attack capacity

--- Assets/_Project/Scripts/EnemyMovement.cs
+++ Assets/_Project/Scripts/EnemyMovement.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Spounka
@@ -9,18 +6,22 @@ namespace Spounka
     {
         #region Variables
 
-        [SerializeField] private float _movementSpeed = 3.0f, _distanceThreshold = 1.0f;
+        [SerializeField] private float _movementSpeed = 3.0f, _distanceThreshold = 1.5f;
         [SerializeField] private Transform _target = null;
 
         private Transform _transform = null;
         private Rigidbody2D _rb = null;
+        private EnemyAttack _attack;
+        private HealthSystem enemyHealth;
 
         #endregion
 
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
+            _attack = GetComponent<EnemyAttack>();
             _transform = transform;
+            enemyHealth = _target.GetComponent<HealthSystem>();
         }
 
         private void FixedUpdate()
@@ -31,7 +32,11 @@ namespace Spounka
         private void MoveToTarget()
         {
             if (Vector2.Distance(_target.position, _transform.position) < _distanceThreshold)
+            {
+                _attack.Attack(enemyHealth);
                 return;
+            }
+
             var direction = (Vector2)(_target.position - _transform.position).normalized;
             var velocity = direction * _movementSpeed * Time.fixedDeltaTime;
             _rb.MovePosition(_rb.position + velocity);
