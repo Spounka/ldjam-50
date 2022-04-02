4c12b06d33dec8b06839e9ae1909776594587855
Nazih Boudaakkar <boudaakkranazih@gmail.com>
2022-04-02 15:05:29 +0100

    Import TextMeshPro

---
+++ Assets/_Project/Scripts/UI/HealthUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spounka
+{
+    public class HealthUI : MonoBehaviour
+    {
+    	#region Variables
+	
+	
+	
+    	#endregion
+
+
+        private void Start()
+        {
+        
+        }
+
+
+        private void Update()
+        {
+        
+        }
+    }
+}
