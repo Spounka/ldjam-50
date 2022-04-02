8f713c537cdf5f12b1dc2c9af3236bd33e910632
Nazih Boudaakkar <boudaakkranazih@gmail.com>
2022-04-02 16:30:33 +0100

    created an enemy spawner

---
+++ Assets/_Project/Scripts/EnemySpawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Spounka
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+    	#region Variables
+	
+	
+	
+    	#endregion
+
+
+        private void Start()
+        {
+        
+        }
+
+
+        private void Update()
+        {
+        
+        }
+    }
+}
