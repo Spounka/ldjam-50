17116f22e96718c295aa8acfd1e72bc20c07f105
Nazih Boudaakkar <boudaakkranazih@gmail.com>
2022-04-03 02:22:45 +0100

    Added CameraFollow System

---
+++ Assets/_Project/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spounka
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private float _followSpeed;
+        [SerializeField] private Transform _followTarget;
+        [SerializeField] private Vector3 _offset;
+
+        #endregion
+
+        private void LateUpdate()
+        {
+            transform.position =
+                Vector3.Lerp(transform.position, _followTarget.position + _offset, _followSpeed * Time.deltaTime);
+        }
+    }
+}
